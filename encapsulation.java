6a9cbb7a6483e813b5c479a60d8731d990cdea24
Kavya2801-boddu <kavyagoud.boddu@mail.techsemble.us>
2025-12-17 22:10:53 -0600

    Added all java files

---
+++ encapsulation.java
@@ -0,0 +1,32 @@
+class encapsulation{
+    String name;
+    String designation;
+    private int age;
+    private double salary;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+
+    public class Person{
+        public static void main(String[] args){
+         Person p1 = new Person;
+         p1.age();
+
+        }
+
+    }
+}
